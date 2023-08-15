7f4f15812e922982d63b721df5e0c468acc0d594
rocha19 <rochaaqueologia@outlook.com>
2023-08-15 16:03:59 -0300

    feat: add email validate to entities

---
+++ src/server/domain/utils/email.ts
@@ -0,0 +1,22 @@
+import { InvalidEmailError } from "@/server/domain/errors"
+import { Either, left, right } from "@/shared"
+
+export class Email {
+  public readonly value: string
+  private constructor(email: string) {
+    this.value = email
+  }
+  public static register(email: string): Either<InvalidEmailError, Email> {
+    if (!Email.validator(email)) {
+      return left(new InvalidEmailError(email))
+    }
+    return right(new Email(email))
+  }
+  public static validator(email: string): boolean {
+    if (!email) return false
+    if (email.trim().length < 2 || email.trim().length > 50) {
+      return false
+    }
+    return true
+  }
+}
