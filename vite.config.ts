563ea86149ca5659d12daba69eb17303c39026b9
rocha19 <rochaaqueologia@outlook.com>
2023-08-15 15:58:12 -0300

    chore: configure the test environment

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['**/*.{spec,test}.ts'],
+    exclude: [
+      'node_modules',
+      'dist',
+      '.idea',
+      '.git',
+      '.cache',
+      'temp',
+      'coverage'
+    ],
+    watchExclude: ['**/node_modules/**, **/dist/**, **/temp/**'],
+    watch: false,
+    hookTimeout: 60000,
+    testTimeout: 60000
+    // onConsoleLog: (log: string, type: 'stdout' | 'stderr') => false
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@tests': path.resolve(__dirname, './__tests__')
+    }
+  },
+  esbuild: {
+    target: 'node18'
+  },
+  plugins: [react()],
+})
